5ec02f2aab573d8c9c4808dbbe4e434728ccb2f6
FORCHA <forchapearl1@gmail.com>
2021-11-19 12:33:29 +0100

    Add files via upload

---
+++ build/precache-manifest.c45fefe403140a4914e097beda2f3304.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04310b41583d596884174f0b9e1149d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9df67b64e69a569c6c53",
+    "url": "/static/css/main.1db91c02.chunk.css"
+  },
+  {
+    "revision": "e1667d923fbf5bba1033",
+    "url": "/static/js/2.e995efda.chunk.js"
+  },
+  {
+    "revision": "9df67b64e69a569c6c53",
+    "url": "/static/js/main.0b46f3b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
