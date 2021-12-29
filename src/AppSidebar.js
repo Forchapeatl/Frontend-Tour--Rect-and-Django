718abf7caf951b9018e20f1f4d2592aec21f87ac
FORCHA <forchapearl1@gmail.com>
2021-12-29 05:32:34 +0100

    Update AppSidebar.js

--- src/AppSidebar.js
+++ src/AppSidebar.js
@@ -7,8 +7,8 @@ export default function AppSidebar(props) {
         <h2>Our Tours</h2>
       </header>
       <section className="App-sidebar__intro">
-        <h3>Explore California</h3>
-        <p>What type of California experience do you want to have? Are you looking for an exciting adventure? A chance to relax and get away from it all? Or maybe you're looking for a chance to do something entirely new and enriching! As you can see, we've got you covered. We offer tours that are as diverse as California itself! Select any of our tours learn more about pricing and available options.</p>
+        <h3>Explore Cameroon</h3>
+        <p>What type of Cameroon experience do you want to have? Are you looking for an exciting adventure? A chance to relax and get away from it all? Or maybe you're looking for a chance to do something entirely new and enriching! As you can see, we've got you covered. We offer tours that are as diverse as California itself! Select any of our tours learn more about pricing and available options.</p>
       </section>
     </section>
   );
